c2826de75b9edacf6d13926c776b2cff9853d19a
Vivian <55993344+weiaquarius@users.noreply.github.com>
2020-12-27 21:34:11 +0800

    update

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ var about = document.querySelector('.about');
 
 var one = document.querySelector('.one');
 var two = document.querySelector('.two');
-var three = document.querySelector('.three');
+// var three = document.querySelector('.three');
 var four = document.querySelector(' .four');
 
 var type1 = document.querySelector('.type1');
 var type2 = document.querySelectorAll('.type2');
-var type3 = document.querySelectorAll('.type3');
+// var type3 = document.querySelectorAll('.type3');
 var type4 = document.querySelector('.type4');
 
 var project1 =document.querySelector('#project1');
 var project2 =document.querySelector('#project2');
-var project3 =document.querySelector('#project3');
+// var project3 =document.querySelector('#project3');
 var project4 =document.querySelector('#project4');
 
 var stateportfolio = { name : "portfolio"};
@@ -25,12 +25,12 @@ var stateabout = { name : "about"};
 
 var stateone = { name : "one"};
 var statetwo = { name : "two"};
-var statethree = { name : "three"};
+// var statethree = { name : "three"};
 var statefour = { name : "four"};
 
 var state_one = { name : "type1"};
 var state_two = { name : "type2"};
-var state_three = { name : "type3"};
+// var state_three = { name : "type3"};
 var state_four= { name : "type4"};
 
 // var currentState = history.state;
@@ -54,9 +54,9 @@ window.addEventListener("popstate",function(e){
     else if (state === "two"){
         _two();
     }
-    else if (state === "three"){
-        _three();
-    }
+    // else if (state === "three"){
+    //     _three();
+    // }
     else if(state === "four"){
         _four();
     }
@@ -66,9 +66,9 @@ window.addEventListener("popstate",function(e){
     else if (state === "type2"){
         _type2();
     }
-    else if (state === "type3"){
-        _type3();
-    }
+    // else if (state === "type3"){
+    //     _type3();
+    // }
     else if (state === "type4"){
         _type4();
     }
@@ -88,9 +88,11 @@ function _portfolio(){
             project1.classList.remove('mode');
         }else if(project2.className === "mode"){
             project2.classList.remove('mode');
-        }else if(project3.className === "mode"){
-            project3.classList.remove('mode');
-        }else{
+        }
+        // else if(project3.className === "mode"){
+        //     project3.classList.remove('mode');
+        // }
+        else{
             project4.classList.remove('mode');
         }
     }
@@ -111,9 +113,11 @@ function _about(){
             project1.classList.remove('mode');
         }else if(project2.className === "mode"){
             project2.classList.remove('mode');
-        }else if(project3.className === "mode"){
-            project3.classList.remove('mode');
-        }else{
+        }
+        // else if(project3.className === "mode"){
+        //     project3.classList.remove('mode');
+        // }
+        else{
             project4.classList.remove('mode');
         }
     }
@@ -166,28 +170,28 @@ function _two(){
     window.document.documentElement.scrollTop = 0;
 }
 
-three.addEventListener('click',_three)
-function _three(){
-    window.history.pushState(statethree,"project3","");
-    if (portfoliobtn.className === "selected"){
-        portfolio.classList.remove('mode');
-        portfoliobtn.classList.remove('selected');
-        pos.classList.remove('run');
-    }else if(aboutbtn.className === "selected"){
-        about.classList.remove('mode');
-        aboutbtn.classList.remove('selected');
-    }else{
-        if(project1.className === "mode"){
-            project1.classList.remove('mode');
-        }else if(project2.className === "mode"){
-            project2.classList.remove('mode');
-        }else{
-            project4.classList.remove('mode');
-        }
-    }
-    project3.classList.add('mode');
-    window.document.documentElement.scrollTop = 0;
-}
+// three.addEventListener('click',_three)
+// function _three(){
+//     window.history.pushState(statethree,"project3","");
+//     if (portfoliobtn.className === "selected"){
+//         portfolio.classList.remove('mode');
+//         portfoliobtn.classList.remove('selected');
+//         pos.classList.remove('run');
+//     }else if(aboutbtn.className === "selected"){
+//         about.classList.remove('mode');
+//         aboutbtn.classList.remove('selected');
+//     }else{
+//         if(project1.className === "mode"){
+//             project1.classList.remove('mode');
+//         }else if(project2.className === "mode"){
+//             project2.classList.remove('mode');
+//         }else{
+//             project4.classList.remove('mode');
+//         }
+//     }
+//     project3.classList.add('mode');
+//     window.document.documentElement.scrollTop = 0;
+// }
 
 four.addEventListener('click',_four)
 function _four(){
@@ -233,30 +237,32 @@ function _type2(){
     window.history.pushState(state_two,"project2","");
         if (project1.className === "mode"){
             project1.classList.remove('mode');
-        }else if (project3.className === "mode"){
-            project3.classList.remove('mode');
-        }else{
+        }
+        // else if (project3.className === "mode"){
+        //     project3.classList.remove('mode');
+        // }
+        else{
             project4.classList.remove('mode');
         }
         project2.classList.add('mode');
         window.document.documentElement.scrollTop = 0;
 }
 
-for (let i = 0; i < type3.length; i++) {
-    type3[i].addEventListener("click", _type3);
-}
-function _type3(){
-    window.history.pushState(state_three,"project3","");
-    if (project1.className ==="mode"){
-        project1.classList.remove('mode');
-    }else if (project2.className ==="mode"){
-        project2.classList.remove('mode');
-    }else{
-        project4.classList.remove('mode');
-    }
-    project3.classList.add('mode');
-    window.document.documentElement.scrollTop = 0;
-}
+// for (let i = 0; i < type3.length; i++) {
+//     type3[i].addEventListener("click", _type3);
+// }
+// function _type3(){
+//     window.history.pushState(state_three,"project3","");
+//     if (project1.className ==="mode"){
+//         project1.classList.remove('mode');
+//     }else if (project2.className ==="mode"){
+//         project2.classList.remove('mode');
+//     }else{
+//         project4.classList.remove('mode');
+//     }
+//     project3.classList.add('mode');
+//     window.document.documentElement.scrollTop = 0;
+// }
 
 type4.addEventListener('click',_type4)
 function _type4(){
